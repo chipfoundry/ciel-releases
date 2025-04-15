335f99d36c4619236ce595dc8185cd13000d5668
Mohamed Gaber <me@donn.website>
2025-04-15 16:34:03 +0000

    ci: api cache script

---
+++ cache_api.py
@@ -0,0 +1,62 @@
+# Copyright 2025 The American University in Cairo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import json
+import argparse
+
+from pathlib import Path
+
+from ciel.source import GitHubReleasesDataSource
+from ciel.common import mkdirp, date_to_iso8601
+
+parser = argparse.ArgumentParser(description="A program that takes an output directory and a single input.")
+parser.add_argument("-O", "--output_dir", help="The directory to write output to")
+parser.add_argument("target_repo", help="The target input repo (in the format <owner>/<repo>)")
+
+args = parser.parse_args()
+
+download_path = Path(args.output_dir)
+data_source = GitHubReleasesDataSource(args.target_repo)
+for pdk in ["sky130", "gf180mcu", "ihp_sg13g2"]:
+    versions = data_source.get_available_versions(pdk)
+    base =  download_path / pdk
+    mkdirp(base)
+    manifest_versions = []
+    for version in versions:
+        entry = {
+            "version": version.name,
+            "date": date_to_iso8601(version.commit_date),
+        }
+        if version.prerelease:
+            entry["prerelease"] = True
+        manifest_versions.append(entry)
+    manifest = {
+        "version": 1,
+        "pdk": pdk,
+        "versions": manifest_versions
+    }
+    with open(base / "manifest.json", "w") as f:
+        json.dump(manifest, f)
+    for version in versions:
+        version_manifest = {
+            "version": 1,
+            "assets": []
+        }
+        version_base = base / version.name
+        mkdirp(version_base)
+        _, assets = data_source.get_downloads_for_version(version)
+        for asset in assets:
+            version_manifest["assets"].append(asset.__dict__)
+        with open(version_base / "manifest.json", "w") as f:
+            json.dump(version_manifest, f)
